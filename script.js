98fd10d2a805a8bbdeaf78272b995d5f56320b91
Igor.Solovyov <googlplohoy@gmail.com>
2020-03-26 14:51:27 +0200

    git

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ start();
 
 function chooseExpenses() {
   for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходовы в этом месяце",""),
+    let a = prompt("Введите обязательную статью расходовы в этом месяце", ""),
         b = prompt("Во сколько обойдется?", "");
 
     if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null 
@@ -31,19 +31,32 @@ function chooseExpenses() {
 }
 chooseExpenses();
 
-appData.moneyPerDay = (appData.money / 30).toFixed();
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-  console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень достатка");
-} else {
-  console.log("Произошла ошибка");
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.money / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
+detectDayBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectLevel();
+
+function chooseOptExpenses() {
+  for (let i = 1; i <= 3; i++) {
+    let b = prompt("Статья необязательных расходов?", "");
+    appData.optionalExpenses[i] = b;
+  }
+}
+chooseOptExpenses();
 
 function checkSavings() {
   if (appData.savings == true) {
